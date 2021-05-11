08158d2dbd8d55b003cef030293035fad5539eec
Abuzar Amin <abuzaramin706@gmail.com>
2021-05-11 11:11:30 +0500

    Added sensors , ads and finger print lock

---
+++ SensorsAndSecurity/GyroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GyroViewController.swift
+//  SensorsAndSecurity
+//
+//  Created by Abuzar Amin on 05/05/2021.
+//  Copyright © 2021 Abuzar Amin. All rights reserved.
+//
+
+import UIKit
+
+class GyroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
